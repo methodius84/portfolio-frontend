61ab498c0a96bc5b0766793f7422fb0994101adc
Kirill Kobunov <wasd-sport@yandex.ru>
2024-04-22 04:22:20 +0300

    media mobile tryout #1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,16 +2,16 @@
   <div class="about-wrapper">
     <div class="about">
       <h1 class="about-title">About me</h1>
-      <div class="skills">
-        <h2 class="skills-title">My skills</h2>
-        <div class="skills-wrapper">
-          <div class="proficiencies">
-            <h3 class="proficiencies-title">Proficiencies</h3>
-            <p class="proficiencies-text">
-              As a developer, I am always eager to learn new things.<br>
-              My main advantage is my perfectionism, I never leave my job unfinished.
-            </p>
-          </div>
+      <div class="skills-wrapper">
+        <div class="proficiencies">
+          <h3 class="proficiencies-title">Proficiencies</h3>
+          <p class="proficiencies-text">
+            As a developer, I am always eager to learn new things.<br>
+            My main advantage is my perfectionism, I never leave my job unfinished.
+          </p>
+        </div>
+        <div class="skills">
+          <h2 class="skills-title">My skills</h2>
           <div class="skill-list">
             <ul class="list">
               <li>PHP</li>
@@ -34,7 +34,7 @@
 
 .about-wrapper
   position: relative
-  height: 100vh
+  height: 100dvh
   background-image: url("../assets/images/girl-background.png")
   background-repeat: no-repeat
   background-size: cover
@@ -45,21 +45,37 @@
       display: flex
       justify-content: center
       color: $primary-h-text-color
-    .skills
+    .skills-wrapper
       margin-top: 150px
-      display: flex
-      flex-direction: column
-      .skills-title
+      overflow: hidden
+      //display: flex
+      //flex-direction: column
+      .proficiencies
+        float: left
+        display: block
+        margin-top: 46px
+        margin-left: 20%
+        color: $primary-h3-text-color
+      .proficiencies-text
+        font-size: 16px
+        text-align: left
+        color: $primary-paragraph-small-text2
+        width: 200px
         display: flex
-        justify-content: end
-        margin-right: 22%
-      .skills-wrapper
-        display: flex
+        flex-wrap: wrap
+      .skills
+        float: left
+        display: block
+        margin-left: 15%
         justify-content: space-between
-        flex-direction: row
+        flex-direction: column
+        .skills-title
+          display: flex
+          justify-content: end
+          margin-right: 22%
         .skill-list
           background-color: rgba($background-3, 0.4)
-          margin-top: 20px
+          margin-top: 40px
           margin-right: 10%
           padding: 30px
           transform: rotate3d(1, 4, -3, 15deg)
@@ -72,17 +88,6 @@
               font-family: 'MartianMono', sans-serif
               font-weight: 500
               padding-inline: 1ch
-        .proficiencies
-          margin-top: 46px
-          margin-left: 25%
-          color: $primary-h3-text-color
-        .proficiencies-text
-          font-size: 16px
-          text-align: left
-          color: $primary-paragraph-small-text2
-          width: 200px
-          display: flex
-          flex-wrap: wrap
 
 
 //@media (min-width: 1024px)
